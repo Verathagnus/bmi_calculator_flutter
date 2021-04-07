f8b80fbc0767c25b843a2752f6fd67fe7982ce25
FlutLab.io <>
2021-04-07 19:39:09 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -4,10 +4,12 @@ import 'package:flutter/src/painting/box_decoration.dart';
 import 'package:flutter/src/painting/border_radius.dart';
 import 'package:flutter/src/widgets/basic.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
 const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -26,7 +28,21 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: activeCardColour),
+                  child: ReusableCard(
+                      colour: activeCardColour
+                      cardChild: Column(
+                          children: <Widget>[
+                              Icon(
+                                  FontAwesomeIcons.mars,
+                                  size: 80.0,
+                              ),
+                              SizedBox(height: 15.0),
+                              Text('MALE', style: TextStyle(
+                                  fontSize: 10.0, color: Color(0xFFBDBE98),
+                              ),),
+                          ],
+                      ),
+                      ),
                 ),
                 Expanded(
                   child: ReusableCard(colour: activeCardColour),
@@ -62,12 +78,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
+  ReusableCard({@required this.colour, this.cardChild});
   final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
