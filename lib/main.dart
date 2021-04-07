81a4f308f6f7e386c3178b95180bc4b67f677158
FlutLab.io <>
2021-04-07 17:25:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-            primaryColor: Colors.red,
+            primaryColor: Color(#0A0B21),
             accentColor: Colors.purple,
         ),
       home: InputPage(),
